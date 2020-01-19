bb729d3c449be0f43500cc2360d58a28ca7216fc
Anna <462243@e1.ru>
2020-01-19 21:09:09 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -39,8 +39,14 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        address2 = wd.find_element_by_name("address2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, mobilephone=mobilephone,
-                       workphone=workphone, secondaryphone=secondaryphone)
+                       workphone=workphone, secondaryphone=secondaryphone, address=address, address2=address2,
+                       email=email, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
@@ -181,9 +187,12 @@ class ContactHelper:
                 cells = row.find_elements_by_tag_name("td")
                 firstname = cells[1].text
                 lastname = cells[2].text
+                address = cells[3].text
+                all_emails = cells[4].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
-                                                  all_phones_from_home_page=all_phones))
+                                                  all_phones_from_home_page=all_phones,
+                                                  all_emails_from_home_page=all_emails, address=address))
         return list(self.contact_cache)
 
--- model/contact.py
+++ model/contact.py
@@ -6,7 +6,8 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None,
                  email3=None, homepage=None,  address2=None, phone2=None, notes=None, id=None, homephone=None,
-                 mobilephone=None, workphone=None, secondaryphone=None, all_phones_from_home_page=None):
+                 mobilephone=None, workphone=None, secondaryphone=None, all_phones_from_home_page=None,
+                 all_emails_from_home_page=None,):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -30,6 +31,7 @@ class Contact:
         self.workphone = workphone
         self.secondaryphone = secondaryphone
         self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
         self.id = id
 
     def __repr__(self):

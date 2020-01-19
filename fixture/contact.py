168d54318aee250fb5a4604ea7b7959832da5468
Anna <462243@e1.ru>
2020-01-19 23:29:50 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице.

--- fixture/contact.py
+++ fixture/contact.py
@@ -53,8 +53,8 @@ class ContactHelper:
         self.open_contact_view_by_index(index)
         text = wd.find_element_by_id("content").text
         homephone = re.search("H: (.*)", text).group(1)
-        workphone = re.search("W: (.*)", text).group(1)
         mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
         secondaryphone = re.search("P: (.*)", text).group(1)
         return Contact(homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
 

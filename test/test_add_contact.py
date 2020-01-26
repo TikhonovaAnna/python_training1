b14010feaf800bf87a17a243500510e86a63179e
Anna <462243@e1.ru>
2020-01-26 22:20:45 +0300

    Задание №18: Реализовать генераторы тестовых данных (contacts)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import pytest
-import random
-import string
-from data.add_contact import constant as testdata
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_new(app, contact):
+
+def test_add_new(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     new_contacts = app.contact.get_contact_list()

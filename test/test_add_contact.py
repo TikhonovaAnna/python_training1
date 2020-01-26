d6f674c0c6c3b3331552a366ae98725dd8ff22b6
Anna <462243@e1.ru>
2020-01-26 23:18:56 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов (contacts)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,8 @@
 from model.contact import Contact
 
 
-def test_add_new(app, data_contacts):
-    contact = data_contacts
+def test_add_new(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     new_contacts = app.contact.get_contact_list()

7e614b2ede8e21b98ae1d9d0fba5d0429c1b923e
Anna <462243@e1.ru>
2020-01-28 23:39:14 +0300

    Фикстура для взаимодействия с базой данных (dbfixture подсвечивается красным)

---
+++ fixture/db.py
@@ -0,0 +1,14 @@
+import pymysql.connection
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password="")
+
+
+    def destroy(self):
+        self.connectoin.close()

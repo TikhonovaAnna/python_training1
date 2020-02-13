4b5504861b4246211d637d292ec623076dbfd663
Anna <462243@e1.ru>
2020-02-13 22:57:49 +0300

    задание №29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,12 +5,14 @@ import allure
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    with allure.step("STEP1"):
+    with allure.step("Given a group list"):
         old_groups = db.get_group_list()
-    app.group.create(group)
-    # assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step("When I add a new group to the list"):
+        app.group.create(group)
+    with allure.step("Then the new group list is equal to the old list with the added group"):
+        # assert len(old_groups) + 1 == app.group.count()
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
